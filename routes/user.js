3f77ffcee9fd755007b23df7a15d41ec557f592b
Alex Lebedyev <aresobus@gmail.com>
2017-01-31 11:21:49 -0500

    add user profile page route, loginRequired helper method

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var router = express.Router();
-const authHelpers = require('../auth/auth-helpers');
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const express = require('express');
+const router = express.Router();
 
+const authHelpers = require('../auth/auth-helpers');
+
+/* GET user profile page. */
+// add route here
 router.get('/', authHelpers.loginRequired, (req, res, next) => {
   res.render('user/index', {
     user: req.user.dataValues
+//if someone is trying to log in - call function from ../auth/auth-helpers to check password, if succes - render user/index page
   });
 });
+
+
 
 
 module.exports = router;
